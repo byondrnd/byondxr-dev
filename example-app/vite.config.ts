b2db7d5244e2bc3d4c9b21a10ae977062fc6d065
Pavel Ravits <vim55k@gmail.com>
2024-04-16 23:16:03 +0300

    add-babel

--- example-app/vite.config.ts
+++ example-app/vite.config.ts
@@ -3,5 +3,24 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	clearScreen: false,
+	plugins: [
+		react({
+			babel: {
+				plugins: [
+					[
+						'@byondxr/babel-plugin',
+						{
+							componentWrappers: {
+								memo: { importName: 'memo', importSource: '@byondxr/react-utils' },
+								observer: { importName: 'observer', importSource: '@legendapp/state/react' },
+							},
+						},
+					],
+				],
+			},
+			include: [/.\/src\/.+\.tsx?$/],
+			exclude: [/.\/packages\/.+\.tsx?$/],
+		}),
+	],
 })
