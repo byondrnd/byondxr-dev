e276b34ac589b676433b2d0b51c279207f23bbd3
Pavel Ravits <vim55k@gmail.com>
2024-04-18 16:45:03 +0300

    clean

--- packages/example-app/src/main.tsx
+++ packages/example-app/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RecoilRoot } from 'recoil'
-import type { Ref } from 'react'
 import App from './app'
 import './index.css'
 
