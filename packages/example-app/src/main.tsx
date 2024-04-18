d78c984fbb39774d3d789621376f9204fa0aeba7
Pavel Ravits <vim55k@gmail.com>
2024-04-18 16:43:05 +0300

    clean babel

--- packages/example-app/src/main.tsx
+++ packages/example-app/src/main.tsx
@@ -5,21 +5,22 @@ import type { Ref } from 'react'
 import App from './app'
 import './index.css'
 
-export const A = ({ children }: { children: string }) => {}
+type Props = { children: string }
+export const A = ({ children }: Props) => {}
 
-type HtmlProps = any
-export const HtmlComponent = (
-	{ children, style, className, renderFirst, ...props }: HtmlProps,
-	ref: Ref<HTMLDivElement>
-) => {
-	let b: any = {}
-	let a = b.get()
-	return (
-		<div ref={ref} {...props} style={style} className={className} data-component="HtmlComponent">
-			{renderFirst && children}
-		</div>
-	)
-}
+// type HtmlProps = any
+// export const HtmlComponent = (
+// 	{ children, style, className, renderFirst, ...props }: HtmlProps,
+// 	ref: Ref<HTMLDivElement>
+// ) => {
+// 	let b: any = {}
+// 	let a = b.get()
+// 	return (
+// 		<div ref={ref} {...props} style={style} className={className} data-component="HtmlComponent">
+// 			{renderFirst && children}
+// 		</div>
+// 	)
+// }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
